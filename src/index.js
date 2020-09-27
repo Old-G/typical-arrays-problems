2c46a3877e15d6e59836361d81fbfbda1017b2a0
Gleb <actimel091@gmail.com>
2020-09-27 14:43:44 +0300

    feat: redid the task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 exports.min = function min(array) {
     if (!array || !array.length) return 0;
-    return array.reduce((min, curr) => Math.min(min, curr));
+    return Math.min.apply(Math, array);
 };
 
 exports.max = function max(array) {
     if (!array || !array.length) return 0;
-    return array.reduce((max, curr) => Math.max(max, curr));
+    return Math.max.apply(Math, array);
 };
 
 exports.avg = function avg(array) {
     if (!array || !array.length) return 0;
-    return array.reduce((acc, curr) => acc + curr, 0) / array.length;
+    return array.reduce((a, b) => a + b, 0) / array.length;
 };
